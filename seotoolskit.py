3f7e326b0048c32dc4c4685fd6429e6cfb7e3050
neeraj795 <neerajkumar.wh@gmail.com>
2024-08-11 01:25:09 +0530

    Update seotoolskit.py

--- seotoolskit.py
+++ seotoolskit.py
@@ -21,15 +21,6 @@ st.set_page_config(
 <meta name="title" content="TITLE">
 <meta name="description" content="DESCRIPTION">
 <meta property="og:type" content="website">
-<meta property="og:url" content="URL">
-<meta property="og:title" content="TITLE">
-<meta property="og:description" content="DESCRIPTION">
-<meta property="og:image" content="IMAGEURL>
-<meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="URL">
-<meta property="twitter:title" content="TITLE">
-<meta property="twitter:description" content="DESCRIPTION">
-<meta property="twitter:image" content="IMAGEURL">
     page_icon="🔍",
     layout="wide",
     initial_sidebar_state="expanded",
